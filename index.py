bc85d12a9a802de559fdab666a1fc8198d3a0d70
subrat-dwi <chubukov789@gmail.com>
2026-04-08 23:57:06 +0530

    Implement PDF processing and embedding functionality; update requirements and add Docker Compose for Qdrant

--- index.py
+++ index.py
@@ -1,11 +1,35 @@
 import pathlib
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
 
 path = pathlib.Path(__file__).parent.resolve() / "data_files/The_Courage_to_be_Disliked_How_to_Change_Your_Life_and_Achieve_Real.pdf"
+load_dotenv()
 
 def load_pdf(file_path: str):
     # load the PDF file using PyPDFLoader
     loader = PyPDFLoader(file_path)
     docs = loader.load()
     return docs
+
+def split_documents(documents, chunk_size=1000, chunk_overlap=200):
+    # split the documents into smaller chunks using RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    chunks = text_splitter.split_documents(documents)
+    return chunks
+
+def embed_documents(chunks):
+    # embed the chunks using OpenAIEmbeddings
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-large",
+    )
+
+    # create a vector store using QdrantVectorStore and store the embedded chunks
+    vector_store = QdrantVectorStore.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        url= "http://localhost:6333",
+        collection_name= "rag-learning"
+    )
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from index import load_pdf
+from index import load_pdf, split_documents, embed_documents
 
 def main():
     file_path = "data_files/The_Courage_to_be_Disliked_How_to_Change_Your_Life_and_Achieve_Real.pdf"
     documents = load_pdf(file_path)
     print(f"Loaded {len(documents)} documents from the PDF.")
-    print(f"First document content: {documents[10].page_content[:500]}")  # Print the first 500 characters of the first document
+
+    chunks = split_documents(documents)
+    print(f"Split the documents into {len(chunks)} chunks.")
+
+    embed_documents(chunks)
+    print("Embedded the chunks and stored them in the vector store.")
 
 
 if __name__ == "__main__":
